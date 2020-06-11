bba978a402afe474255d88b31a24435ff3c7bc49
braph <braph93@gmx.de>
2020-06-11 15:41:17 +0200

    updated hash.hpp

--- lib/hash.hpp
+++ lib/hash.hpp
@@ -1,21 +1,44 @@
 #ifndef LIB_HASH
 #define LIB_HASH
 
+#include <cstdint>
+
 /* Some constexpr hash algorithms.
  * Taken from http://www.cse.yorku.ca/~oz/hash.html
  */
 
 namespace Hash {
 
-template<typename TInt>
-constexpr TInt lose_lose(const char* s) {
+constexpr uint64_t lose_lose(const char* s) noexcept {
   return
     (*s) ? (
-      lose_lose<TInt>(s + 1) + TInt(*s)
+      lose_lose(s + 1) + uint64_t(*s)
     ) : (
       0
     );
 }
+
+constexpr uint64_t djb2(const char* s) noexcept {
+  return
+    (*s) ? (
+      djb2(s + 1) * 33 + uint64_t(*s)
+    ) : (
+      5381
+    );
+}
+
+constexpr uint64_t sdbm(const char* s) noexcept {
+  return
+    (*s) ? (
+      sdbm(s + 1) * 65599 + uint64_t(*s)
+    ) : (
+      0
+    );
+}
+
+inline uint64_t lose_lose(const std::string& s) noexcept { return lose_lose(s.c_str()); }
+inline uint64_t djb2     (const std::string& s) noexcept { return djb2(s.c_str());      } 
+inline uint64_t sdbm     (const std::string& s) noexcept { return sdbm(s.c_str());      }
 
 } // namespace Hash
 
