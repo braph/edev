d727eb50c79de265d3b91e6c6183e285ff4e27ec
braph <braph93@gmx.de>
2021-01-06 01:41:15 +0100

    basic_string

--- lib/cstring.hpp
+++ lib/cstring.hpp
@@ -3,7 +3,6 @@
 
 #include <string>
 #include <cstring>
-#include <cwchar>
 #include <ctime>
 #include <cstdio>
 #include <cstdarg>
@@ -12,97 +11,53 @@ static inline const char* ensure_string(const char* s) { return (s ? s : ""); }
 
 // ============================================================================
 
-struct CString {
-  CString(const CString& s) noexcept
+template<class TChar = char>
+struct Basic_CString {
+  Basic_CString(const Basic_CString& s) noexcept
     : _s(s._s)
     , _len(s._len)
   {}
 
-  CString(const char* s) noexcept
+  Basic_CString(const TChar* s) noexcept
     : _s(s)
-    , _len(std::string::npos)
+    , _len(std::basic_string<TChar>::npos)
   {}
 
   template<typename T>
-  CString(const char* s, T len) noexcept
+  Basic_CString(const TChar* s, T len) noexcept
     : _s(s)
     , _len(static_cast<size_t>(len))
   {}
 
-  CString(const std::string& s) noexcept
+  Basic_CString(const std::basic_string<TChar>& s) noexcept
     : _s(s.c_str())
     , _len(s.length())
   {}
 
-  const char* c_str() const noexcept {
+  const TChar* c_str() const noexcept {
     return _s;
   }
 
-  operator const char*() const noexcept {
-    return _s;
-  }
-
-  size_t length() noexcept {
-    if (_len == std::string::npos)
-      _len = std::strlen(_s);
-    return _len;
-  }
-
-  bool empty() noexcept {
-    return !_len;
-  }
-
-protected:
-  const char* _s;
-  size_t _len;
-};
-
-// ============================================================================
-
-struct CWString {
-  CWString(const CWString& s) noexcept
-    : _s(s._s)
-    , _len(s._len)
-  {}
-
-  CWString(const wchar_t* s) noexcept
-    : _s(s)
-    , _len(std::wstring::npos)
-  {}
-
-  template<typename T>
-  CWString(const wchar_t* s, T len) noexcept
-    : _s(s)
-    , _len(static_cast<size_t>(len))
-  {}
-
-  CWString(const std::wstring& s) noexcept
-    : _s(s.c_str())
-    , _len(s.length())
-  {}
-
-  const wchar_t* c_str() const noexcept {
-    return _s;
-  }
-
-  operator const wchar_t*() const noexcept {
+  operator const TChar*() const noexcept {
     return _s;
   }
 
   size_t length() noexcept {
-    if (_len == std::wstring::npos)
-      _len = std::wcslen(_s);
+    if (_len == std::basic_string<TChar>::npos)
+      _len = std::char_traits<TChar>::length(_s);
     return _len;
   }
 
   bool empty() noexcept {
-    return !_len;
+    return length() == 0;
   }
 
 protected:
-  const wchar_t* _s;
+  const TChar* _s;
   size_t _len;
 };
+
+using CString = Basic_CString<char>;
 
 // ============================================================================
 
@@ -112,15 +67,20 @@ protected:
 template<size_t N>
 struct temp_sprintf {
 
-#if defined(__GNUC__) || defined(__clang__)
+#if !defined(NDEBUG) && (defined(__GNUC__) || defined(__clang__))
   __attribute__((__format__(__printf__, 2, 3)))
-#endif
   temp_sprintf(const char* fmt, ...) noexcept {
     va_list ap;
     va_start(ap, fmt);
     vsprintf(buf, fmt, ap);
     va_end(ap);
   }
+#else
+  template<typename ... T>
+  temp_sprintf(const char* fmt, T ... args) noexcept {
+    sprintf(buf, fmt, args ...);
+  }
+#endif
 
   operator const char*() const noexcept { return buf; }
 
@@ -136,15 +96,20 @@ private:
 template<size_t N>
 struct temp_snprintf {
 
-#if defined(__GNUC__) || defined(__clang__)
+#if !defined(NDEBUG) && (defined(__GNUC__) || defined(__clang__))
   __attribute__((__format__(__printf__, 2, 3)))
-#endif
   temp_snprintf(const char* fmt, ...) noexcept {
     va_list ap;
     va_start(ap, fmt);
     vsnprintf(buf, N, fmt, ap);
     va_end(ap);
   }
+#else
+  template<typename ... T>
+  temp_snprintf(const char* fmt, T ... args) noexcept {
+    snprintf(buf, N, fmt, args ...);
+  }
+#endif
 
   operator const char*() const noexcept { return buf; }
 
